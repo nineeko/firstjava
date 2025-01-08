5b892fb4d9a08ff3ba61312e302d18f2eab07e62
nineeko <gkxm1013@naver.com>
2025-01-08 16:33:26 +0900

    "first commit"

---
+++ src/Calculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        //1. 데이터 자유 입력
+        Scanner in = new Scanner(System.in);
+        System.out.println("start number input: ");
+            int start = in.nextInt();
+        System.out.println("end number input: ");
+            int end = in.nextInt();
+            int sum = 0;
+        //2. 데이터 처리  //3. 데이터 결과 저장
+
+        for(int i = start; i <= end; i++) {
+           if(i % 2 == 0) {
+               sum += i; //sum=sum+i;
+           }
+        }
+
+        //4. 결과 출력
+        System.out.println("결과값 =>"+ sum);
+
+    }
+}
---
+++ src/first.java
@@ -0,0 +1,8 @@
+public class first {
+    public static void main(String[] args) {
+        System.out.println("첫 번째 프로그램입니다.");
+        System.out.println("두 번째 프로그램입니다. 한 줄에 여러 문장을 출력합니다.");
+        System.out.println("세번째 프로그램입니다.\n줄을 바꾸어 출력합니다.\n여기까지 출력하고\n줄을 바꿉니다.");
+    }
+
+}
