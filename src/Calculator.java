53ca84dcde1f59215956319ec9bb06be7a3c0ebb
nineeko <gkxm1013@naver.com>
2025-01-08 17:18:30 +0900

    two commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -14,6 +14,7 @@ public class Calculator {
 
         for(int i = start; i <= end; i++) {
            if(i % 2 == 0) {
+               System.out.println("i= "+i);
                sum += i; //sum=sum+i;
            }
         }
